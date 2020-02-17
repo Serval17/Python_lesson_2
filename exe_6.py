ab1ce4b490d14505fbd714fe50eb4eaa046ec8f6
Serval17 <serval_17@mail.ru>
2020-02-18 03:40:00 +1000

    exercise 6-10

---
+++ exe_6.py
@@ -0,0 +1,56 @@
+# Задание 6
+#
+# integer_number = 351
+# sum = 0
+# while integer_number>0:
+#      sum = sum + integer_number%10
+#      integer_number = integer_number//10
+# print('Сумма цифр числа:', sum)
+
+
+
+# Задание 7
+#
+# integer_number = 3245
+# mult = 1
+# while integer_number>0:
+#      mult *= integer_number%10
+#      integer_number = integer_number//10
+# print('Произведение цифр числа', mult)
+
+
+
+# Задание 8
+#
+# integer_number = 2544153
+# while integer_number>0:
+#     if integer_number%10 == 5:
+#         print('Yes')
+#         break
+#     integer_number = integer_number//10
+# else: print('No')
+
+
+
+# Задание 9
+#
+# integer_number = 658723
+# max = 0
+# while integer_number>0:
+#     if integer_number%10 > max:
+#         max = integer_number%10
+#     integer_number = integer_number//10
+# print('Mаксимальная цифра:', max)
+
+
+
+# Задание 10
+#
+# integer_number = 4152658453
+# qfive = 0
+# while integer_number>0:
+#     if integer_number%10 == 5:
+#         qfive += 1
+#     integer_number = integer_number//10
+# print('Количество пятерок в числе:', qfive)
+#
